5142ffc2a6087a514d0575eafb5eb890c242ac85
noex.home <noex.home@googlemail.com>
2012-12-02 16:00:30 +0000

    Added new fragment concept including down support API level 7.

---
+++ DailyExpensesFragments/src/com/daily/expenses/ItemInsertActivity.java
@@ -0,0 +1,53 @@
+package com.daily.expenses;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ItemInsertActivity extends FragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_item_insert);
+        /* Red[128224] Ensure compatibility of pre API 11 devices - action bar */
+        // Show the Up button in the action bar.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        	getActionBar().setDisplayHomeAsUpEnabled(true);
+    	}
+
+        // savedInstanceState is non-null when there is fragment state
+        // saved from previous configurations of this activity
+        // (e.g. when rotating the screen from portrait to landscape).
+        // In this case, the fragment will automatically be re-added
+        // to its container so we don't need to manually add it.
+        // For more information, see the Fragments API guide at:
+        //
+        // http://developer.android.com/guide/components/fragments.html
+        //
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(ItemInsertFragment.ARG_ITEM_ID,
+                    getIntent().getStringExtra(ItemInsertFragment.ARG_ITEM_ID));
+            ItemInsertFragment fragment = new ItemInsertFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.item_insert_container, fragment)
+                    .commit();
+        }
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_item_insert, menu);
+		return true;
+	}
+}
---
+++ DailyExpensesFragments/src/com/daily/expenses/database/RecordsDatabaseHelper.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.daily.expenses.database;
+
+/**
+ * @author No3x
+ *
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class RecordsDatabaseHelper extends SQLiteOpenHelper {
+
+  private static final String DATABASE_NAME = "records.db";
+  private static final int DATABASE_VERSION = 1;
+
+  public RecordsDatabaseHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+
+  // Method is called during creation of the database
+  @Override
+  public void onCreate(SQLiteDatabase database) {
+    RecordsTable.onCreate(database);
+  }
+
+  // Method is called during an upgrade of the database,
+  // e.g. if you increase the database version
+  @Override
+  public void onUpgrade(SQLiteDatabase database, int oldVersion,
+      int newVersion) {
+	  RecordsTable.onUpgrade(database, oldVersion, newVersion);
+  }
+} 
