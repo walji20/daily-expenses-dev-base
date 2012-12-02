60bcc52bf3ed0876bf00652e94805a79b5af65d2
noex.home@googlemail.com <noex.home@googlemail.com@e09a3b74-3872-f2d2-75d8-06fddd3d395f>
2012-12-02 16:00:30 +0000

    Added new fragment concept including down support API level 7.

---
+++ trunk/DailyExpensesFragments/src/com/daily/expenses/dummy/DummyContent.java
@@ -0,0 +1,169 @@
+package com.daily.expenses.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    static {
+        // Add 3 sample items.
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Item 1", "An item is a real complex thing. it can be little or small .. or long .. or ............................................................asd a sd asd as"));
+        addItem(new DummyItem("2", "Item 2", "Android :) "));
+        addItem(new DummyItem("3", "Item 3"));
+        
+    }
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public String id;
+        public String title;
+        public String text;
+
+        public DummyItem(String id, String content) {
+            this.id = id;
+            this.title = content;
+            this.text = null;
+        }
+        
+        public DummyItem(String id, String content, String text) {
+        	this.id = id;
+        	this.title = content;
+        	this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
+    }
+}
