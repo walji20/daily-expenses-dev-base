2190f447622c243f895f389ec5b35989b8bb2378
noex.home@googlemail.com <noex.home@googlemail.com@e09a3b74-3872-f2d2-75d8-06fddd3d395f>
2012-12-16 13:24:10 +0000

    Data for view from ContenProvider.

--- trunk/DailyExpensesFragments/src/com/daily/expenses/RecordDetailFragment.java
+++ trunk/DailyExpensesFragments/src/com/daily/expenses/RecordDetailFragment.java
@@ -3,10 +3,14 @@ package com.daily.expenses;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.CursorLoader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,6 +37,11 @@ public class RecordDetailFragment extends SherlockFragment {
      */
     private RecordsContent.Record mItem;
 
+    private Spinner mCategory;
+    private EditText mSummaryText;
+    private EditText mDescriptionText;
+    
+    private Uri recordUri;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -43,41 +52,13 @@ public class RecordDetailFragment extends SherlockFragment {
     @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+	     
 		if (getArguments().containsKey(ARG_ITEM_ID)) {
 			// Load the dummy content specified by the fragment
 			// arguments. In a real-world scenario, use a Loader
 			// to load content from a content provider.
 			mItem = RecordsContent.ITEM_MAP.get(getArguments().getString(
 					ARG_ITEM_ID));
-
-			Bundle extras = this.getActivity().getIntent().getExtras();
-			Uri todoUri;
-			// Check from the saved Instance
-			todoUri = (savedInstanceState == null) ? null
-					: (Uri) savedInstanceState
-							.getParcelable(RecordsContentProvider.CONTENT_ITEM_TYPE);
-
-			// Or passed from the other activity
-			if (extras != null) {
-				todoUri = extras.getParcelable(RecordsContentProvider.CONTENT_ITEM_TYPE);
-				Log.d("todo", "todoUri: " + todoUri);
-				String[] projection = { RecordsTable.COLUMN_ID,
-										RecordsTable.COLUMN_DESCRIPTION,
-										RecordsTable.COLUMN_CATEGORY 
-										};
-				
-				Cursor cursor = getActivity().getContentResolver().query(todoUri, projection, null, null, null);
-				
-				if (cursor != null) {
-					Log.d("cursor ", "" + cursor);
-					cursor.moveToFirst();
-					String category = cursor.getString(cursor.getColumnIndexOrThrow(RecordsTable.COLUMN_CATEGORY));
-
-				}
-			  // Always close the cursor
-		      cursor.close();
-			}
 		}
 	}
 
@@ -85,16 +66,56 @@ public class RecordDetailFragment extends SherlockFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_record_detail, container, false);
+        
+        
+    	mCategory = (Spinner) rootView.findViewById(R.id.category);
+		mSummaryText = (EditText) rootView.findViewById(R.id.record_edit_summary);
+		mDescriptionText = (EditText) rootView.findViewById(R.id.record_edit_description);
+		Button mRecordEditButton = (Button) rootView.findViewById(R.id.record_edit_button);
+		
+		Bundle extras = this.getActivity().getIntent().getExtras();
+		
+		// Check from the saved Instance
+		recordUri = (savedInstanceState == null) ? null : (Uri) savedInstanceState .getParcelable(ARG_ITEM_ID);
 
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-        	if(mItem.text != null) {
-        		((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.text);
-        	} else {
-        		((TextView) rootView.findViewById(R.id.item_detail)).setText("edit content by long press an item on the left.");
-			}
-        }
+		// Or passed from the other activity
+		if (extras != null) {
+			 String itemId = (String) extras.getString(ARG_ITEM_ID);
+			 recordUri = Uri.parse(RecordsContentProvider.CONTENT_URI + "/" + itemId);
+			 fillData(recordUri);
+		}  
 
         return rootView;
     }
+    
+private void fillData(Uri uri) {
+	Log.d("todo", "todoUri: " + recordUri);
+	String[] projection = { RecordsTable.COLUMN_ID,
+							RecordsTable.COLUMN_CATEGORY,
+							RecordsTable.COLUMN_SUMMARY,
+							RecordsTable.COLUMN_DESCRIPTION,
+							};
+	
+	Cursor cursor = getActivity().getContentResolver().query(recordUri, projection, null, null, null);
+	
+	if (cursor != null) {
+		  cursor.moveToFirst();
+		  
+          String category = cursor.getString(cursor.getColumnIndexOrThrow(RecordsTable.COLUMN_CATEGORY));
+
+          for (int i = 0; i < mCategory.getCount(); i++) {
+              String s = (String) mCategory.getItemAtPosition(i);
+              if (s.equalsIgnoreCase(category)) {
+                      mCategory.setSelection(i);
+              }
+          }
+
+          mSummaryText.setText(cursor.getString(cursor.getColumnIndexOrThrow(RecordsTable.COLUMN_SUMMARY)));
+          mDescriptionText.setText(cursor.getString(cursor.getColumnIndexOrThrow(RecordsTable.COLUMN_DESCRIPTION)));
+
+	}
+  // Always close the cursor
+  cursor.close();
+}
+
 }
