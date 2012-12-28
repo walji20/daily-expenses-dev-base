8c4f585e03b8c210602c65df9d57793904d419c9
noex.home@googlemail.com <noex.home@googlemail.com@e09a3b74-3872-f2d2-75d8-06fddd3d395f>
2012-12-28 23:24:23 +0000

    Implemented new dialog concept for SpinnerEditCategoryDialogFragment.

--- trunk/DailyExpensesFragments/src/com/daily/expenses/dialogs/SpinnerEditCategoryDialogFragment.java
+++ trunk/DailyExpensesFragments/src/com/daily/expenses/dialogs/SpinnerEditCategoryDialogFragment.java
@@ -4,6 +4,7 @@ import com.daily.expenses.R;
 import com.daily.expenses.RecordDetailFragment;
 import com.daily.expenses.contentprovider.DailyContentProvider;
 import com.daily.expenses.database.DailyTables;
+import com.daily.expenses.dialogs.EditDateDialogFragment.EditDateDialogListener;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -26,18 +27,53 @@ import android.widget.EditText;
 public class SpinnerEditCategoryDialogFragment extends DialogFragment {
 	EditText mCategoryTitle;
 	Button mCategoryDelete;
-	int mCurrentCategoryId;
-	/* must be hold in class to communicate with listeners */
-	Cursor mCurrentCategoryCursor = null;
-	ContentResolver mContentResolver = null;
-	//TODO: improve
-	// ugly way to inform spinner about changed data
-	RecordDetailFragment attachedFragment = null;
+	private int mCurrentCategoryId;
+	private String mCurrentCategoryTitle;
+	/* The activity that creates an instance of this dialog fragment must
+	 * implement this interface in order to receive event callbacks.
+	 * Each method passes the DialogFragment in case the host needs to query it. */
+	public interface SpinnerEditCategoryDialogListener {
+		public void onSpinnerEditCategoryDialogPositiveClick(int categoryId, String categoryTitle);
+		public void onSpinnerEditCategoryDialogNeutralClick(int categoryId, String categoryTitle);
+		public void onSpinnerEditCategoryDialogNegativeClick(DialogFragment dialog);
+		public void onSpinnerEditCategoryDialogDeleteClick(int categoryId);
+	}
+	
+	// Use this instance of the interface to deliver action events
+	SpinnerEditCategoryDialogListener mListener = new SpinnerEditCategoryDialogListener() {
+		
+		@Override
+		public void onSpinnerEditCategoryDialogPositiveClick(int categoryId, String categoryTitle) {
+			//Should always overridden by interface implementing class
+		}
+		
+		@Override
+		public void onSpinnerEditCategoryDialogNeutralClick(int categoryId, String categoryTitle) {
+			//Should always overridden by interface implementing class
+		}
+		
+		@Override
+		public void onSpinnerEditCategoryDialogNegativeClick(DialogFragment dialog) {
+			//Should always overridden by interface implementing class
+		}
 
-	public static SpinnerEditCategoryDialogFragment newInstance( int currentCategoryId ) {
+		@Override
+		public void onSpinnerEditCategoryDialogDeleteClick(int categoryId) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+
+	public void setListener(SpinnerEditCategoryDialogListener listener) {
+		mListener = listener;
+	}
+	 
+    
+	public static SpinnerEditCategoryDialogFragment newInstance( int currentCategoryId, String currentCategoryTitle ) {
 		SpinnerEditCategoryDialogFragment p = new SpinnerEditCategoryDialogFragment();
 		Bundle args = new Bundle();
 		args.putInt("currentCategoryId", currentCategoryId);
+		args.putString("currentCategoryTitle", currentCategoryTitle);
 		p.setArguments(args);
 		return p;
 	}
@@ -45,9 +81,11 @@ public class SpinnerEditCategoryDialogFragment extends DialogFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		mCurrentCategoryId = getArguments().getInt("currentCategoryId");
+		mCurrentCategoryTitle = getArguments().getString("currentCategoryTitle");
 		super.onCreate(savedInstanceState);
 	}
-
+	
+	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		LayoutInflater inflater = LayoutInflater.from(getActivity());
@@ -55,79 +93,52 @@ public class SpinnerEditCategoryDialogFragment extends DialogFragment {
 		mCategoryTitle = (EditText) v.findViewById(R.id.dialog_category_title);
 		mCategoryDelete = (Button) v.findViewById(R.id.dialog_category_delete);
 		
-		attachedFragment = (RecordDetailFragment) getFragmentManager().findFragmentById(R.id.record_detail_container);
 		
 		mCategoryDelete.setOnClickListener( new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				
-				if( mCurrentCategoryId > -1 ) {
-					mContentResolver.delete(Uri.parse(DailyContentProvider.CATEGORIES_CONTENT_URI + "/" + mCurrentCategoryId), null, null);
-					
-					if(attachedFragment != null) {
-						attachedFragment.refreshData();
-					}
-					getDialog().cancel();
-				}
-			}
-		});
-		String[] categoryProjection = { DailyTables.TABLE_CATEGORIES_COLUMN_ID, DailyTables.TABLE_CATEGORIES_COLUMN_TITLE };
-		final Uri categoryUri = Uri.parse(DailyContentProvider.CATEGORIES_CONTENT_URI + "/" + mCurrentCategoryId);
-		mContentResolver = getActivity().getContentResolver();
-		mCurrentCategoryCursor = mContentResolver.query( categoryUri, categoryProjection, null, null, null);
 		
-		if(mCurrentCategoryCursor.moveToFirst()) {
+			@Override
+			public void onClick(View v) {
+				
+				mListener.onSpinnerEditCategoryDialogDeleteClick(mCurrentCategoryId);
+				getDialog().cancel();
+				
+//				if( mCurrentCategoryId > -1 ) {
+//					mContentResolver.delete(Uri.parse(DailyContentProvider.CATEGORIES_CONTENT_URI + "/" + mCurrentCategoryId), null, null);
+//					
+//					if(attachedFragment != null) {
+//						attachedFragment.refreshData();
+//					}
+//					getDialog().cancel();
+//				}
+			}
+				
+		});
+
 			
-			mCategoryTitle.setText(mCurrentCategoryCursor.getString(mCurrentCategoryCursor.getColumnIndexOrThrow(DailyTables.TABLE_CATEGORIES_COLUMN_TITLE)));
+			mCategoryTitle.setText( mCurrentCategoryTitle );
+			
 			return new AlertDialog.Builder(getActivity()).setTitle("Category Name...").setView(v).setCancelable(true).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					Log.d("", "Dialog confirmed");
+					mListener.onSpinnerEditCategoryDialogPositiveClick(mCurrentCategoryId, mCurrentCategoryTitle);
 					
-					ContentValues values = new ContentValues();
-					values.put(DailyTables.TABLE_CATEGORIES_COLUMN_TITLE, mCategoryTitle.getText().toString());
-					mContentResolver.update(categoryUri, values, null, null);
-					mCurrentCategoryCursor.close();
-					
-					if(attachedFragment != null) {
-						attachedFragment.refreshData();
-					}
 				}
 			}).setNegativeButton("Abort", new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
 					Log.d("", "Dialog abort");
+					
+					dialog.cancel();
 				}
 			}).setNeutralButton("As new Category", new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					Log.d("", "Dialog neutral");
 					
-					if(attachedFragment != null) {
-						Uri currentRecordUri = attachedFragment.getRecordUri();
-						
-						int currentRecordId = Integer.parseInt(currentRecordUri.getLastPathSegment());
-						if( currentRecordId > -1 ) {
-							ContentValues categoryValues = new ContentValues();
-							categoryValues.put(DailyTables.TABLE_CATEGORIES_COLUMN_TITLE, mCategoryTitle.getText().toString());
-							Uri categorySavedUri = mContentResolver.insert(DailyContentProvider.CATEGORIES_CONTENT_URI, categoryValues);
-							int categorySavedId = Integer.parseInt(categorySavedUri.getLastPathSegment());
-							if( categorySavedId > -1 ) {
-								
-								ContentValues recordValues = new ContentValues();
-								recordValues.put(DailyTables.TABLE_RECORDS_COLUMN_CATEGORY_TYPE, categorySavedId);
-								mContentResolver.update(Uri.parse(DailyContentProvider.RECORDS_CONTENT_URI + "/" + currentRecordId), recordValues, null, null);
-							}
-						}
-						attachedFragment.refreshData();
-					}
+					mListener.onSpinnerEditCategoryDialogNeutralClick(mCurrentCategoryId, mCategoryTitle.getText().toString());
 					dialog.cancel();
 				}
 			}).create();
-		} else {
-			return null;	
-		}
 	}
 }
